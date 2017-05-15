274f7cc3234b071897710449aaa15d01f874c7b4
mkang_hyd <mkang_hyd@163.com>
2017-05-15 21:40:20 +0800

    95

--- LeetCodeAlgorithm/main.cpp
+++ LeetCodeAlgorithm/main.cpp
@@ -1,15 +1,14 @@
-﻿//	94. Binary Tree Inorder Traversal
+﻿//	95. Unique Binary Search Trees II
 //------------------------------------------------------------------------------//
-//	Given a binary tree, return the inorder traversal of its nodes' values.		//
-//	For example :																//
-//	Given binary tree[1, null, 2, 3],											//
-//	1																			//
-//	 \																			//
-//	  2																			//
-//	 /																			//
-//	3																			//
-//	return[1, 3, 2].															//
-//	Note: Recursive solution is trivial, could you do it iteratively?			//
+//	Given an integer n, generate all structurally unique BST's					//
+//	(binary search trees) that store values 1...n.								//
+//	For example,																//
+//	Given n = 3, your program should return all 5 unique BST's shown below.		//
+//	1      3	 3      2      1												//
+//	 \	  /		/	   / \      \												//
+//	 3   2     1      1   3      2												//
+//	/	/		\                 \												//
+// 2   1         2                 3											//
 //------------------------------------------------------------------------------//
 #include <iostream>
 #include<vector>
@@ -17,8 +16,6 @@
 #include<numeric>
 #include<algorithm>
 #include<functional>
-#include<stack>
-#include<unordered_map>
 // constants
 // function prototype
 using namespace std;
@@ -26,177 +23,44 @@ using namespace std;
 struct TreeNode {
     int val;
     TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-//递归的方法，占用O(n)的栈空间
+//分析同I，递归的做法
 class Solution 
 {
 public:
-	vector<int> inorderTraversal(TreeNode* root) 
+	vector<TreeNode*> generateTrees(int n) 
 	{
-		vector<int> result;
-		inorderTranversal(root, result);
-		return result;
+		if (n == 0) return generate(1, 0);
+		return generate(1, n);
 	}
 private:
-	void inorderTranversal(TreeNode *root, vector<int> &result)
-	{
-		if (root == nullptr)
-			return;
-		inorderTranversal(root->left, result);
-		result.push_back(root->val);
-		inorderTranversal(root->right, result);
-	}
-};
-//迭代的方法，利用一个stack，但会破坏树的结构，不实用
-class Solution2
-{
-public:
-	vector<int> inorderTraversal(TreeNode* root)
-	{
-		vector<int> result;
-		if (root == nullptr)
-			return result;
-		stack<TreeNode *> stack;
-		stack.push(root);
-		while (!stack.empty())
-		{
-			TreeNode *cur = stack.top();
-			if (cur->left)
-			{
-				stack.push(cur->left);
-				cur->left = nullptr;
-			}
-			else
-			{
-				result.push_back(cur->val);
-				stack.pop();
-				if (cur->right)
-					stack.push(cur->right);
-			}
+	vector<TreeNode *> generate(int start, int end) {
+		vector<TreeNode*> subTree;
+		if (start > end) {
+			subTree.push_back(nullptr);
+			return subTree;
 		}
-		return result;
-	}
-};
-//利用一个stack和一个hashtable，不会破坏树结构
-class Solution3
-{
-public:
-	vector<int> inorderTraversal(TreeNode* root)
-	{
-		vector<int> vector;
-		if (!root)
-			return vector;
-		unordered_map<TreeNode *, bool> map;//left child has been visited:true.
-		stack<TreeNode *> stack;
-		stack.push(root);
-		while (!stack.empty())
-		{
-			TreeNode *pNode = stack.top();
-			if (pNode->left && !map[pNode])
-			{
-				stack.push(pNode->left);
-				map[pNode] = true;
-			}
-			else
-			{
-				vector.push_back(pNode->val);
-				stack.pop();
-				if (pNode->right)
-					stack.push(pNode->right);
-			}
-		}
-		return vector;
-	}
-};
-//只用stack，不会破坏树结构，对使用栈来说是最佳的
-class Solution4
-{
-public:
-	vector<int> inorderTraversal(TreeNode* root)
-	{
-		vector<int> result;
-		stack<TreeNode*> stack;
-		TreeNode *cur = root;
-		while (!stack.empty() || cur != nullptr)
-		{
-			if (cur != nullptr)
-			{
-				stack.push(cur);
-				cur = cur->left;
-			}
-			else
-			{
-				cur = stack.top();
-				result.push_back(cur->val);
-				stack.pop();
-				cur = cur->right;
-			}
-		}
-		return result;
-	}
-};
-//Morris中序遍历，空间复杂度O(1)
-//步骤：
-//1. 如果当前节点的左孩子为空，则输出当前节点并将其右孩子作为当前节点。
-//2. 如果当前节点的左孩子不为空，在当前节点的左子树中找到当前节点在中序遍历下的前驱节点。
-//	a) 如果前驱节点的右孩子为空，将它的右孩子设置为当前节点。当前节点更新为当前节点的左孩子。
-//	b) 如果前驱节点的右孩子为当前节点，将它的右孩子重新设为空（恢复树的形状）。输出当前节点。当前节点更新为当前节点的右孩子。
-//3. 重复以上1、2直到当前节点为空。
-class Solution5
-{
-public:
-	vector<int> inorderTraversal(TreeNode* root)
-	{
-		vector<int> result;
-		TreeNode *cur = root, *prev = nullptr;	//当前节点和前驱节点
-		while (cur)
-		{
-			if (cur->left == nullptr)
-			{
-				result.push_back(cur->val);
-				cur = cur->right;
-				prev = cur;
-			}
-			else
-			{
-				TreeNode *node = cur->left;
-				while (node->right != nullptr && node->right != cur)
-					node = node->right;
-				if (node->right == nullptr) { 
-					node->right = cur;
-					cur = cur->left;
-				}
-				else {
-					result.push_back(cur->val);
-					node->right = nullptr;
-					prev = cur;
-					cur = cur->right;
+		for (int k = start; k <= end; k++) {
+			vector<TreeNode*> leftSubs = generate(start, k - 1);
+			vector<TreeNode*> rightSubs = generate(k + 1, end);
+			for (auto i : leftSubs) {
+				for (auto j : rightSubs) {
+					TreeNode *node = new TreeNode(k);
+					node->left = i;
+					node->right = j;
+					subTree.push_back(node);
 				}
 			}
 		}
+		return subTree;
 	}
 };
-void destroy(TreeNode *root)
-{
-	if (root == nullptr)
-		return;
-	destroy(root->left);
-	destroy(root->right);
-	delete root;
-}
 int main(void)
 {
-	Solution5 test;
-	TreeNode *root = new TreeNode(1);
-	root->right = new TreeNode(2);
-	root->right->left = new TreeNode(3);
-	auto x = test.inorderTraversal(root);
-	for_each(x.begin(), x.end(), [](int y) {
-		cout << y << " ";
-	});
-	destroy(root);
+	Solution test;
+	auto x = test.generateTrees(0);
 	cout << endl;
 	
 	// code to keep window open for MSVC++
---
+++ SourceCode/96-Unique Binary Search Trees.cpp
@@ -0,0 +1,54 @@
+﻿//	96. Unique Binary Search Trees
+//------------------------------------------------------------------------------//
+//	Given n, how many structurally unique BST's (binary search trees) that		//
+//	store values 1...n?															//
+//	For example,																//
+//	Given n = 3, there are a total of 5 unique BST's.							//
+//	1      3	 3      2      1												//
+//	 \	  /		/	   / \      \												//
+//	 3   2     1      1   3      2												//
+//	/	/		\                 \												//
+// 2   1         2                 3											//
+//------------------------------------------------------------------------------//
+#include <iostream>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<algorithm>
+#include<functional>
+// constants
+// function prototype
+using namespace std;
+
+//对于一个二叉搜索树集合，以i为根节点的话，则左子树由1~i-1构成，右子树由i+1~n构成
+//数量为左子树个数乘以右子树个数，那么，可以用动态规划来考虑，另f[i]为以i为根节点
+//的BST数量，f[i]=f[i-1]*f[n-i]，令F[n]为n个数的所有BST组合，F[n]=∑f[i]
+class Solution 
+{
+public:
+	int numTrees(int n) 
+	{
+		if (n < 2)
+			return 1;
+		vector<int> F(n + 1, 0);
+		F[0] = F[1] = 1;
+		for (int i = 2; i <= n; i++)
+			for (int j = 1; j <= i; j++)
+				F[i] += F[j - 1] * F[i - j];
+		return F[n];
+	}
+};
+//int main(void)
+//{
+//	Solution test;
+//	cout << test.numTrees(3);
+//	cout << endl;
+//	
+//	// code to keep window open for MSVC++
+//	cin.clear();
+//	while (cin.get() != '\n')
+//		continue;
+//	cin.get();
+//
+//	return 0;
+//}
